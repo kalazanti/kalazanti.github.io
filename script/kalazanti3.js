a61bd4c1f95f215352ba74b5e81588d5e8662401
bucsi <11545252+bucsi@users.noreply.github.com>
2023-02-06 13:21:24 +0100

    remove commented out function

--- script/kalazanti3.js
+++ script/kalazanti3.js
@@ -2,12 +2,5 @@ import PocketBase from "./pocketbase.es.mjs";
 
 const client = new PocketBase("https://kalazanti.fly.dev/");
 
-// window.addEventListener("hashchange", () => {
-//     const id = window.location.hash.slice(2);
-//     main.innerHTML = id;
-// });
-
-
-
 export { client };
 export { location, formatDateTime, leadify, createArticle } from "./utils.js";
