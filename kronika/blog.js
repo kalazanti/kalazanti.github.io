b40f339f685ca10dad1773c303e72aecbe9bc948
11545252+bucsi@users.noreply.github.com <11545252+bucsi@users.noreply.github.com>
2023-02-10 22:14:35 +0100

    only display published articles

--- kronika/blog.js
+++ kronika/blog.js
@@ -1,8 +1,9 @@
 import { client, createArticle } from "../script/kalazanti3.js";
 const main = document.querySelector("main");
 
-const records = await client.records.getFullList("kronika", 200 /* batch size */, {
+const records = await client.collection("kronika").getFullList(200 /* batch size */, {
     sort: "-created",
+    filter: "published = true",
 });
 
 records.forEach(article => {
